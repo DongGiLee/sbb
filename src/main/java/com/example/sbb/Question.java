1f40fc71c328fb97fb6f0c34678069caa7504646
DongDong <37203688+DongGiLee@users.noreply.github.com>
2023-04-05 17:58:44 +0900

    save

---
+++ src/main/java/com/example/sbb/Question.java
@@ -0,0 +1,16 @@
+package com.example.sbb;
+
+import java.time.LocalDateTime;
+
+// javax = 2.x
+// jakarta = 3.x
+
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Question {
+
+}
