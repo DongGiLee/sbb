ff430487300efcae5c35724d117ba131a2da84de
DongDong <37203688+DongGiLee@users.noreply.github.com>
2023-04-12 16:46:18 +0900

    save
    repository

---
+++ src/main/java/com/example/sbb/repository/MemberMapperRepository.java
@@ -0,0 +1,8 @@
+package com.example.sbb.repository;
+
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper // Mapper 용으로 쓸 Repository라고 선언하는 어노테이션
+public interface MemberMapperRepository {
+
+}
